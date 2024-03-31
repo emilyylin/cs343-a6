f857ed9dd01327063088be9a60994d56c25dffca
yy8wong <yy8wong@uwaterloo.ca>
2024-03-31 13:59:30 -0400

    move starter code to header files
    c# Your branch is ahead of 'origin/main' by 1 commit.

---
+++ student.h
@@ -0,0 +1,19 @@
+_Task Student {
+	void main();
+  public:
+	Student( Printer & prt, NameServer & nameServer, WATCardOffice & cardOffice, Groupoff & groupoff,
+			 unsigned int id, unsigned int maxPurchases );
+};
+
+class WATCard {
+	WATCard( const WATCard & ) = delete;	// prevent copying
+	WATCard( const WATCard && ) = delete;
+	WATCard & operator=( const WATCard & ) = delete;
+	WATCard & operator=( const WATCard && ) = delete;
+  public:
+	typedef Future_ISM<WATCard *> FWATCard;	// future watcard pointer
+	WATCard();
+	void deposit( unsigned int amount );
+	void withdraw( unsigned int amount );
+	unsigned int getBalance();
+};
