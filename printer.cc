1c8d7ed43fb5cb42876137c9bcd017aeba42e573
yy8wong <yy8wong@uwaterloo.ca>
2024-04-01 11:31:21 -0400

    add starting printer code

--- printer.cc
+++ printer.cc
@@ -1,1 +1,39 @@
 #include "printer.h" 
+
+Printer::Printer(unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers) :
+    numStudents(numStudents), numVendingMachines(numVendingMachines), numCouriers(numCouriers){
+    // print headers
+    cout << "Parent" << "\t";
+    cout << "Gropoff" << "\t"; // there's a typo in the assignment description
+    cout << "WATOff" << "\t";
+    cout << "Names" << "\t";
+    cout << "Truck" << "\t";
+    cout << "Plant";
+
+    // print students
+    for(unsigned int i =0; i < numStudents; i++){
+        cout << "\t" << "Stud" << i;
+    }
+    // print vending machiines
+    for (unsigned int i = 0; i < numVendingMachines; i++){
+        cout << "\t" << "Mach" << i;
+    }
+    //print couriers
+    for (unsigned int i=0; i < numCouriers; i++){
+        cout << "\t" << "Cour" << i;
+    }
+    cout << endl;
+
+    //print separators
+    int totalSeparators = 6 + numStudents+numVendingMachines+numCouriers;
+    cout << "*******";
+    for(int i =0; i < totalSeparators; i++){
+        cout << "\t" << "*******";
+    }
+    cout << endl;
+}
+
+Printer::~Printer() {
+    //print ending header
+    cout << "***********************";
+}
--- printer.h
+++ printer.h
@@ -1,9 +1,13 @@
 #ifndef PRINTER_H
 #define PRINTER_H
 _Monitor Printer {
+	unsigned int numStudents;
+	unsigned int numVendingMachines;
+	unsigned int numCouriers;
   public:
 	enum Kind { Parent, Groupoff, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
 	Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers );
+	~Printer(); // destructor
 	void print( Kind kind, char state );
 	void print( Kind kind, char state, unsigned int value1 );
 	void print( Kind kind, char state, unsigned int value1, unsigned int value2 );
