1d49ff2bd866d8e70c88f114f8d6f1b58c0ee9d4
yy8wong <yy8wong@uwaterloo.ca>
2024-04-02 15:24:46 -0400

    remove includes for debugging purposes, add printer logic

--- printer.h
+++ printer.h
@@ -1,11 +1,17 @@
 #ifndef PRINTER_H
 #define PRINTER_H
 
+using namespace std;
+#include <map>
+#include <variant>
+
 _Monitor Printer {
+
 	unsigned int numStudents;
 	unsigned int numVendingMachines;
 	unsigned int numCouriers;
   public:
+	
 	enum Kind { Parent, Groupoff, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
 	Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers );
 	~Printer(); // destructor
@@ -15,5 +21,25 @@ _Monitor Printer {
 	void print( Kind kind, unsigned int lid, char state );
 	void print( Kind kind, unsigned int lid, char state, unsigned int value1 );
 	void print( Kind kind, unsigned int lid, char state, unsigned int value1, unsigned int value2 );
+  private:
+	
+	struct StateData{
+		Kind kind;
+		char state;
+	};
+	struct StateValue1Data{
+		Kind kind;
+		char state;
+		unsigned int lid;
+		unsigned int value1;
+	};
+	struct StateValue2Data{
+		Kind kind;
+		char state;
+		unsigned int lid;
+		unsigned int value1;
+		unsigned int value2;
+	};
+	map<Kind, variant<StateData, StateValue1Data, StateValue2Data>> dataBuffer;
 };
 #endif
--- student.h
+++ student.h
@@ -7,16 +7,4 @@ _Task Student {
 			 unsigned int id, unsigned int maxPurchases );
 };
 
-class WATCard {
-	WATCard( const WATCard & ) = delete;	// prevent copying
-	WATCard( const WATCard && ) = delete;
-	WATCard & operator=( const WATCard & ) = delete;
-	WATCard & operator=( const WATCard && ) = delete;
-  public:
-	typedef Future_ISM<WATCard *> FWATCard;	// future watcard pointer
-	WATCard();
-	void deposit( unsigned int amount );
-	void withdraw( unsigned int amount );
-	unsigned int getBalance();
-};
 #endif
